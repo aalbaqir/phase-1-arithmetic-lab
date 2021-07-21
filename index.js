0c48f96fe41406cd7ceca1470582511279d7c5d2
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-07-21 12:08:58 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+// const multiply = (num1=31, num2=2) => {
+//     // let num1 = 31
+//     // let num2 = 2
+//     return num1 * num2
+    
+// }
+
+let multiply
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2
+
+
+let random
+random = Math.floor(Math.random() * 10);
+
+
+let mod
+let num3 = 4
+let num4 = 5
+mod = num3 % num4
+
+let max 
+max= Math.max(0,20)
+// }
